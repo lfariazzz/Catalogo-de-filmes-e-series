684740581836f5c64280dbde6a189fabf3cca200
Levi <levi.farias@aluno.ufca.edu.br>
2025-11-14 15:07:14 -0300

    atuliacao modulo registro

---
+++ models/registro_visualizacao.py
@@ -0,0 +1,6 @@
+class RegistroVisualizacao:
+    def __init__(self, midia, data_visualizacao, progresso, nota_atribuida):
+        self.midia = midia
+        self.data_visualizacao = data_visualizacao
+        self.progresso = progresso
+        self.nota_atribuida = nota_atribuida
