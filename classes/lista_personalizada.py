ed08844d9a54ec86268d3f4420ae2f3dbefeef7f
Levi Farias <levi.farias@aluno.ufca.edu.br>
2025-12-08 16:48:25 -0300

    Adição incompleta do metodo adicionar midia

--- classes/lista_personalizada.py
+++ classes/lista_personalizada.py
@@ -1,3 +1,5 @@
+from classes.midia import Midia
+
 class ListaPersonalizada:
     """Classe responsável pelo armazenamento dos dados das listas personalizadas do usuário"""
     def __init__(self, id_lista, nome, descricao, data_criacao, midias = None):
@@ -7,6 +9,17 @@ class ListaPersonalizada:
         self.descricao = descricao
         self.data_criacao = data_criacao
         self.midias = midias if midias is not None else []
+
+    def adicionar_midia(self, valor):
+        if isinstance(valor, Midia):
+            for midia in self.midias:
+                if valor == midia:
+                    raise ValueError("Essa mídia já está na lista")
+                else:
+                    self.midias.append(valor)
+        else:
+            raise TypeError("Você não inseriu uma mídia.")
+
 
     def editar_lista(self):
         """Método responsável por alterar dados dos parâmetros da classe"""
