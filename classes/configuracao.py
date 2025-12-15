c1f94d3153aae474fb5a4f765d451b2cb538d28c
Levi <levi.farias@aluno.ufca.edu.br>
2025-12-15 20:20:40 -0300

    IMPLEMENTADO REQUISITO 2.7: Implementaçao da classe e json configurações

--- classes/configuracao.py
+++ classes/configuracao.py
@@ -1,5 +1,13 @@
+import json
+
 class Configuracao:
     """Classe responsável pelo controle de algumas configurações de limite e estado do sistema"""
     def __init__(self, caminho_arquivo = "data/settings.json"):
         """Método responsável por definir os parâmetros das configurações a partir da leitura do json settings"""
-        pass
+        with open(caminho_arquivo, 'r', encoding='UTF-8') as arquivo:
+            dados_configuracoes = json.load(arquivo)
+
+            self.nota_recomendada = dados_configuracoes["nota_recomendacao"]
+            self.limite_listas = dados_configuracoes["limite_listas"]
+            self.conversao_horas = dados_configuracoes["conversao_horas"]
+            self.casas_arredondadas_configuraveis = dados_configuracoes["casas_arredondadas_configuraveis"]
